d6f2034c5e9ae355c3e2bda298ce82c0c762ad19
kcrc915 <117868904+kcrc915@users.noreply.github.com>
2026-03-01 16:44:14 -0600

    Add plot for tasks vs duration and checkpoints

---
+++ simulation/Plots/Plots_Aug28/Code/Tasks_VS_Duration.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("tasks_experiment_data.csv")
+
+Tasks = data["Tasks"].values
+
+static = data["StaticDuration"].values
+dynamic_window = data["DynamicWindowDuration"].values
+dynamic_mpc = data["DynamicMPCDuration"].values
+rl = data["RLDuration"].values
+
+line_static = data["StaticCheckpoints"].values
+line_dynamic_window = data["DynamicWindowCheckpoints"].values
+line_dynamic_mpc = data["DynamicMPCCheckpoints"].values
+line_rl = data["RLCheckpoints"].values
+
+bar_width = 0.2
+x = np.arange(len(Tasks))
+
+fig, ax1 = plt.subplots(figsize=(12, 6))
+ax2 = ax1.twinx()
+
+
+ax1.bar(x - bar_width * 1.5, static, bar_width,
+        label='Static Window - Duration', color='blue', alpha=0.7)
+
+ax1.bar(x - bar_width * 0.5, dynamic_window, bar_width,
+        label='Dynamic Window - Duration', color='green', alpha=0.7)
+
+ax1.bar(x + bar_width * 0.5, dynamic_mpc, bar_width,
+        label='Dynamic MPC - Duration', color='red', alpha=0.7)
+
+ax1.bar(x + bar_width * 1.5, rl, bar_width,
+        label='RL - Duration', color='black', alpha=0.7)
+
+
+ax2.plot(x, line_static, marker='o', linestyle='--',
+         color='blue', label='Static Window - Checkpoints')
+
+ax2.plot(x, line_dynamic_window, marker='^', linestyle='--',
+         color='green', label='Dynamic Window - Checkpoints')
+
+ax2.plot(x, line_dynamic_mpc, marker='s', linestyle='--',
+         color='red', label='Dynamic MPC - Checkpoints')
+
+ax2.plot(x, line_rl, marker='d', linestyle='--',
+         color='black', label='RL - Checkpoints')
+
+ax1.set_xlabel('Number of Tasks', fontsize=18)
+ax1.set_ylabel('Duration (s)', fontsize=18)
+ax1.set_ylim(0, 90000)
+
+ax1.set_xticks(x)
+ax1.set_xticklabels(Tasks)
+
+ax2.set_ylabel('Number of Checkpoints', fontsize=18)
+ax2.set_ylim(0, 400)
+ax2.set_yticks(np.linspace(0, 400, 6))
+ax1.set_yticks(np.linspace(0, 90000, 6))
+
+ax1.tick_params(axis='x', labelsize=18)
+ax1.tick_params(axis='y', labelsize=18)
+ax2.tick_params(axis='y', labelsize=18)
+
+ax1.legend(loc='upper left', fontsize=12)
+ax2.legend(loc='upper center', fontsize=12)
+
+plt.tight_layout()
+plt.show()
